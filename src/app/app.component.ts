eddea292833af469ad8b84d13077f3803b73e34b
sashlee <sarahrichie08@gmail.com>
2022-03-07 11:37:31 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HEROES } from '../app/mock-heroes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = 'Tour of Heros';
+  heroes = HEROES;
 }
